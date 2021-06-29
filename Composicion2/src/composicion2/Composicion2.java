a1c0da65ca149ad037022e7b675bd1869f573064
soulotak3 <75744044+soulotak3@users.noreply.github.com>
2021-06-28 22:16:23 -0500

    Add files via upload

--- Composicion2/src/composicion2/Composicion2.java
+++ Composicion2/src/composicion2/Composicion2.java
@@ -11,25 +11,27 @@ import ico.fes.objetos.Monitor;
 import ico.fes.objetos.Mouse;
 import ico.fes.objetos.Teclado;
 
-
 /**
  *
  * @author wesho
  */
 public class Composicion2 {
-      
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Computadora a1 = new Computadora(2017, 2017);
+        System.out.print(a1);
+    CPU b1 = new CPU("amd", "2017", 4);
+        System.out.println(b1);
+    Monitor c1 = new Monitor("Samsung", "2010", 19);
+        System.out.println(c1);
+    Mouse d1 = new Mouse("logitech", "2018", "alambrico");
+        System.out.println(d1);
+    Teclado e1 = new Teclado("RK61", "2016", 61, 13);
+        System.out.println(e1);
         // TODO code application logic here
     }
-    Computadora a1= new Computadora(2017, 2017,null,
-            new CPU("amd", 2017, 4.5,null,
-                    new Monitor("Samsung", 2010, 19,null,
-                            new Mouse("logitech", 2018, "alambrico",null,
-                                    new Teclado("RK61", "2016", 61, 13)))));
     
-    System.out.println( a1.getMarca());
 }
--- Composicion2/src/ico/fes/objetos/Monitor.java
+++ Composicion2/src/ico/fes/objetos/Monitor.java
@@ -27,6 +27,10 @@ public class Monitor {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    public Monitor(String samsung, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     public String getMarca() {
         return marca;
     }
